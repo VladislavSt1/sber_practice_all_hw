3f996177228f3fc9501abf340efd15501e22eeeb
VladislavSt1 <70508810+VladislavSt1@users.noreply.github.com>
2023-04-17 00:34:42 +0300

    Delete hw10 directory

--- hw10/src/main/java/org/example/FileHandler.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import java.io.IOException;
-
-public interface FileHandler {
-    void writeToFile(String fileName, String data) throws IOException;
-    String readFromFile(String fileName) throws IOException;
-}
